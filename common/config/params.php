3fb7a1234fd1c267cc3a0ec35def6e0c8e36a7ad
Josh Pantarotto <jpantarotto@paylease.com>
2019-05-17 11:39:37 -0700

    Re-adding params config file

---
+++ common/config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'adminEmail' => 'bdelatorre@paylease.com',
+    'testEmail' => 'bdelatorre@paylease.com',
+    'senderEmail' => 'meatloafpaylease@gmail.com',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+];
